3bf56da4b08f2f16eae137e5294e5637453d8e3b
Bhushan Amol Anokar <75465521+bhushananokar@users.noreply.github.com>
2025-04-06 23:18:41 +0530

    Update gulpfile.mjs

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -6,3 +6,11 @@ gulp.task('open-app', function(){
   gulp.src('index.html')
   .pipe(open());
 });
+
+// Add a build task for Netlify
+gulp.task('build', function(done){
+  // This task doesn't do anything special since your project might just need
+  // to serve static files without a build process
+  console.log('Build completed');
+  done();
+});
